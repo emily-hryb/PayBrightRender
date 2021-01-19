8c63d76b21cc321a14f3fe50ee159ab9927196df
ehryb <ehryb@EH-MS.Paybright.local>
2021-01-19 17:59:50 -0500

    updated settings

--- PayBrightRender/settings.py
+++ PayBrightRender/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'RenderAPIs.apps.RenderapisConfig'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
